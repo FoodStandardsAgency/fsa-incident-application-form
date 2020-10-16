4578d1dd20b750e99d26affd265f6f21574374f6
Christopher Moss <chris@a6software.co.uk>
2020-10-16 15:39:29 +0100

    [FEATURE] Assembles the products > companies payload

--- incident-application-form/lib/validation/product.js
+++ incident-application-form/lib/validation/product.js
@@ -3,7 +3,7 @@ const { validateProductName } = require("./individual-fields/product-name");
 const { validateBrand } = require("./individual-fields/brand");
 
 module.exports = {
-  validate: ({ productName, brand }, i18n) => {
+  validate: ({ productName, brand, companies }, i18n) => {
     const validatedProductName = validateProductName(productName, i18n);
     const validatedBrand = validateBrand(brand, i18n);
 
@@ -14,6 +14,9 @@ module.exports = {
       validatedFields: {
         ...validatedProductName,
         ...validatedBrand,
+        // TODO - it would be extremely nice to validate companies here, rather than blindly assuming
+        // that the companies argument contains validated companies.
+        companies,
       },
     };
   },
