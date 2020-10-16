fb8a1a21a8c10910622e00653ebeda877d51d6d9
Christopher Moss <chris@a6software.co.uk>
2020-10-16 15:39:29 +0100

    [FEATURE] Assembles the products > companies payload

--- incident-application-form/lib/validation/product.spec.js
+++ incident-application-form/lib/validation/product.spec.js
@@ -25,6 +25,11 @@ describe(`lib/validation/product`, () => {
       {
         productName: "valid",
         brand: "valid",
+        companies: {
+          "123-abc": {
+            name: "valid",
+          },
+        },
       },
       true,
     ],
