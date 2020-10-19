4320443e459d904667473826e22140ff7d982783
Dan <dan.watkinson@gmail.com>
2020-10-19 16:52:55 +0100

    refactoring startup scripts into something a bit nicer

--- pipeline.sh
+++ pipeline.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-# Set and check environment variables and secrets
-for s in $(ls secrets/set_*); do
-    echo " - $s"
-    source $s
-done
+source ./start.sh
 
-if [ -z "$GOV_NOTIFY_API_KEY" ]; then
-    echo "WARNING: GOV NOTIFY API KEY NOT SET"
-fi
-
-cd "$HERE" && docker-compose up -d --build
-
-cd "$HERE" && cd test && npm install && npx cypress run
+cd test && npm install && npx cypress run
 cypressResult=$?
 
 docker-compose down
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source ./setenv.sh
+
+docker-compose up -d --build
