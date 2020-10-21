e832a50e12378fcaa3ef7d49a6f7cbbf14c50274
Dan <dan.watkinson@gmail.com>
2020-10-21 17:57:23 +0100

    fsa styles cause cypress pain, suggests they have usability issues that we likely dont have time to diagnose..

--- test/cypress/integration/user_views_preview.spec.js
+++ test/cypress/integration/user_views_preview.spec.js
@@ -16,53 +16,53 @@ context(
     it(`Visit ${YOUR_DETAILS}, fill in some valid data, submit, prove it exists on the preview`, () => {
       cy.visit(YOUR_DETAILS);
 
-      cy.get('[data-cy="notifier-type"]').select("Industry");
+      cy.get('[data-cy="notifier-type"]').select("Industry", {force: true});
 
-      cy.get('[data-cy="contact-name"]').type("my name here");
+      cy.get('[data-cy="contact-name"]').type("my name here", {force: true});
 
-      cy.get('[data-cy="position"]').type("or should this be role?");
+      cy.get('[data-cy="position"]').type("or should this be role?", {force: true});
 
-      cy.get('[data-cy="organisation"]').type("organisation");
+      cy.get('[data-cy="organisation"]').type("organisation", {force: true});
 
-      cy.get('[data-cy="email"]').type("my.email@somewhere.com");
+      cy.get('[data-cy="email"]').type("my.email@somewhere.com", {force: true});
 
-      cy.get('[data-cy="telephone1"]').type("01234 445 667");
+      cy.get('[data-cy="telephone1"]').type("01234 445 667", {force: true});
 
-      cy.get('[data-cy="address.line1"]').type("line1");
+      cy.get('[data-cy="address.line1"]').type("line1", {force: true});
 
-      cy.get('[data-cy="address.line2"]').type("line2");
+      cy.get('[data-cy="address.line2"]').type("line2", {force: true});
 
-      cy.get('[data-cy="address.town"]').type("town");
+      cy.get('[data-cy="address.town"]').type("town", {force: true});
 
-      cy.get('[data-cy="address.county"]').type("county");
+      cy.get('[data-cy="address.county"]').type("county", {force: true});
 
-      cy.get('[data-cy="address.postcode"]').type("AB1 2CD");
+      cy.get('[data-cy="address.postcode"]').type("AB1 2CD", {force: true});
 
-      cy.get('[data-cy="address.country"]').select("Norway"); //?
+      cy.get('[data-cy="address.country"]').select("Norway", {force: true}); //?
 
-      cy.get('[data-cy="submit"]').click();
+      cy.get('[data-cy="submit"]').click({force: true});
 
       cy.get('[data-cy="nature-of-problem"]').type(
-        "The nature of my problem is salmonella"
+        "The nature of my problem is salmonella", {force: true}
       );
 
       cy.get('[data-cy="action-taken"]').type(
-        "I have notified the authorities"
+        "I have notified the authorities", {force: true}
       );
 
       cy.get('[data-cy="distribution-details"]').type(
-        "This was distributed to the South East"
+        "This was distributed to the South East", {force: true}
       );
 
-      cy.get('[data-cy="illness-details"]').type("People are being sick");
+      cy.get('[data-cy="illness-details"]').type("People are being sick", {force: true});
 
-      cy.get('[data-cy="local-authority-notified"]').type("Yes. I told Dave.");
+      cy.get('[data-cy="local-authority-notified"]').type("Yes. I told Dave.", {force: true});
 
       cy.get('[data-cy="additional-information"]').type(
-        "I also have told the police."
+        "I also have told the police.", {force: true}
       );
 
-      cy.get('[data-cy="submit"]').click();
+      cy.get('[data-cy="submit"]').click({force: true});
 
       // shortcut; this is all meant to be on the session
       //  so for individual pages we can prove their content in isolation
