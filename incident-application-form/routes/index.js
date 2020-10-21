494540dddc207b9e2de096dc6950314b112bfd5d
Christopher Moss <chris@a6software.co.uk>
2020-10-21 11:13:22 +0100

    [FEATURE] Index page translation

--- incident-application-form/routes/index.js
+++ incident-application-form/routes/index.js
@@ -1,11 +1,22 @@
 var express = require("express");
 var router = express.Router();
 
+const template = "index";
+
+const languageCode = "en";
+const pageTranslations = require(`${__dirname}/../translations/${template}.json`);
+
 const routes = require(`${__dirname}/../routes/routes.json`);
+
+const i18n = {
+  languageCode,
+  ...pageTranslations,
+};
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", {
+    i18n,
     links: [
       {
         url: routes.YOUR_DETAILS,
