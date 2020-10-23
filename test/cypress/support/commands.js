c3f56b15154b38d126b8963df7da87f17acfdeb8
danWatkinson <dan.watkinson@gmail.com>
2020-10-23 10:49:12 +0100

    Release Friday morning's changes into production (#77)
    * [TEST] Additional your-details checks
    
    * changing name of branch that corresponds to production environment (#75)
    
    * [TEST] Adds additional tests for details of incident
    
    Co-authored-by: Christopher Moss <chris@a6software.co.uk>

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -1,3 +1,6 @@
+import "./commands/fill-in-contact-details";
+import "./commands/fill-in-details-of-incident";
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
