f9fe8b2a45bf218798e1af9fa451efea02c9f140
Christopher Moss <chris@a6software.co.uk>
2020-10-23 08:26:45 +0100

    [TEST] Additional check of start button on index page

--- test/cypress/integration/access_online_form.spec.js
+++ test/cypress/integration/access_online_form.spec.js
@@ -1,14 +1,27 @@
-const ONLINE_FORM = Cypress.config('baseUrl');
+const ONLINE_FORM = Cypress.config("baseUrl");
 
-context('As a service user, I want to access an online form to report an incident, so that I can fulfil my legal obligation in a convenient way.', () => {
+context(
+  "As a service user, I want to access an online form to report an incident, so that I can fulfil my legal obligation in a convenient way.",
+  () => {
+    beforeEach(() => {
+      // catches, logs and ignores any load-errors on the page..
+      // can probably delete this but worth including in 1st template in case we need it..
+      cy.on("uncaught:exception", (err, runnable) => {
+        console.log(err.stack);
+        return false;
+      });
+    });
 
-  beforeEach(() => {
-    // catches, logs and ignores any load-errors on the page..
-    // can probably delete this but worth including in 1st template in case we need it..
-    cy.on('uncaught:exception', (err, runnable) => { console.log(err.stack); return false; });
-  });
+    it(`Visit ${ONLINE_FORM}`, () => {
+      cy.visit(ONLINE_FORM);
 
-  it(`Visit ${ONLINE_FORM}`, () => {
-    cy.visit(ONLINE_FORM);
-  });
-});
+      cy.get("[data-cy=start-button]").should("contain", "Start");
+    });
+
+    it(`Visit ${ONLINE_FORM}/cy`, () => {
+      cy.visit(`${ONLINE_FORM}/cy`);
+
+      cy.get("[data-cy=start-button]").should("contain", "Dechrau");
+    });
+  }
+);
